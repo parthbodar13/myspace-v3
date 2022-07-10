11d2a548ee01d93b975e356543bb90876396aef6
Jignesh <parth.bodar.c@gmail.com>
2022-07-11 02:19:14 +0530

    @Entityscan

--- masterdataservice/src/main/java/com/nyspace/masterdataservice/MasterDataServiceApplication.java
+++ masterdataservice/src/main/java/com/nyspace/masterdataservice/MasterDataServiceApplication.java
@@ -2,8 +2,10 @@ package com.nyspace.masterdataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({ "com.myspace.commonservice.entity" })
 public class MasterDataServiceApplication {
 
 	public static void main(String[] args) {
